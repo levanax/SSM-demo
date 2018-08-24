afeae25ff2babb94492558ae59d7caa5e77d963b
Levan <levana.xue@hotmail.com>
2018-08-25 07:20:01 +0800

    添加配置文件

--- src/main/java/top/xuebiao/Application.java
+++ src/main/java/top/xuebiao/Application.java
@@ -3,11 +3,13 @@ package top.xuebiao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 @SpringBootApplication
+@PropertySource(value="classpath:application-private.properties",encoding="utf-8")
 public class Application {
 	
 	public static void main(String[] args){
